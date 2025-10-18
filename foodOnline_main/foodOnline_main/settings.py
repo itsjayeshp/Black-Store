10ea2ecaab7820b19f8d54377307769a6deddff1
itsjayeshp <jayeshpatidar1310@gmail.com>
2025-10-18 07:10:58 +0530

    error fixed in html

--- foodOnline_main/foodOnline_main/settings.py
+++ foodOnline_main/foodOnline_main/settings.py
@@ -119,8 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-
+# Static files configuration
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # For production collection
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Points to foodOnline_main/static/
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
